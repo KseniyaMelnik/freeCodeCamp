e413e479577bcec20290c91a4b719529f44ea0a1
KseniyaMelnik <70837696+KseniyaMelnik@users.noreply.github.com>
2021-12-03 21:51:45 +0300

    Create 01.js

---
+++ basic data structures/01.js
@@ -0,0 +1,14 @@
+let yourArray = [
+    {
+     name: "Kseniya",
+     age: 29,
+     isMarried: true 
+     },
+      {
+     name: "Stanislav",
+     age: 30,
+     isMarried: true 
+     }, 
+    
+    "family", 2, true
+]
